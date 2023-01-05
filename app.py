dd5b6e2295c7f5b41a7da5aab04ee8c4063942b1
Anamika Sinha <aniverma2006@gmail.com>
2023-01-05 17:45:24 -0500

    UPdated DB path

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask,render_template, request
+from flask import Flask, render_template, request
 import sqlite3
 import logging
-#conn=sqlite3.connect('FEEDBACK.db')
-#c=conn.cursor()
-app=Flask(__name__)
 
-#initialize logging
-LOG_FILE_NAME='feedbacklog.txt'
+# conn=sqlite3.connect('FEEDBACK.db')
+# c=conn.cursor()
+app = Flask(__name__)
+
+# initialize logging
+LOG_FILE_NAME = 'feedbacklog.txt'
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
                     filename=LOG_FILE_NAME,
                     filemode='w')
 
-#home page and link to feedback form
+
+# home page and link to feedback form
 @app.route("/")
 def home():
     contents_line1 = "<h1>We Appreciate Your Feedback</h1>"
@@ -21,24 +23,22 @@ def home():
     contents = contents_line1 + "<br>" + contents_line2
     return contents
 
-#getting data from feedback form
-@app.route("/feedback",methods=['POST','GET'])
-def feedback():
-    feedback_form=[]
-    if request.method == 'POST':
 
+# getting data from feedback form
+@app.route("/feedback", methods=['POST', 'GET'])
+def feedback():
+    feedback_form = []
+    if request.method == 'POST':
         feedback_form.append(request.form['name'])
         feedback_form.append(request.form['email'])
         feedback_form.append(request.form['phone'])
         feedback_form.append(request.form['gender'])
         feedback_form.append(request.form['course'])
         feedback_form.append(request.form['rate'])
-       # print(feedback_form)
-        logging.info('collected data from feedback form ' + ",".join(feedback_form))
+        # print(feedback_form)
+        logging.info('collected data from feedback form----> ' + ",".join(feedback_form))
         write_feedback_data(feedback_form)
         get_feedback_data()
-
-
 
     return render_template('feedback.html')
 
@@ -49,14 +49,14 @@ def get_response():
     return "DOne"
 
 
-
 def write_feedback_data(feedback_form):
-
     #  conn = db_connect(FEEDBACK.db)
     # cur = conn.cursor()
     conn = sqlite3.connect('FEEDBACK.db')
     cur = conn.cursor()
+
     # logging.info('Connected to ' + db_name)
+
     sql = """insert into feedback_data(
           'name',
           'email',
@@ -65,22 +65,21 @@ def write_feedback_data(feedback_form):
           'course',
           'rate') values(?,?,?,?,?,?)"""
     # verifying the sql statement for debug
-   # print(sql)
-    cur.execute(sql,[x for x in feedback_form])
+    # print(sql)
+    cur.execute(sql, [x for x in feedback_form])
     cur.execute("commit")
     refno = cur.lastrowid
     print(refno)
     cur.close()
     conn.close()
-    #logging.info("DB commit successful")
+    # logging.info("DB commit successful")
     return refno
 
 
 def get_feedback_data():
-
     #  conn = db_connect(FEEDBACK.db)
     # cur = conn.cursor()
-    conn = sqlite3.connect('C:\DS\project\Flask_demo\FEEDBACK.db')
+    conn = sqlite3.connect('FEEDBACK.db')
     cur = conn.cursor()
     cur.execute("select * from feedback_data")
     result = cur.fetchall()
@@ -88,7 +87,9 @@ def get_feedback_data():
         print(data)
     cur.close()
     conn.close()
-    #logging.info("DB commit successful")
+    # logging.info("DB commit successful")
     return True
+
+
 if __name__ == "__main__":
-    app.run(debug=True, port="5001")
+    app.run(debug=True, port='5001')
--- db.py
+++ db.py
@@ -1,9 +1,14 @@
 import sqlite3
-conn=sqlite3.connect('FEEDBACK.db')
-c=conn.cursor()
+from sqlite3 import Connection
+
+conn: Connection = sqlite3.connect('FEEDBACK.db')
+c = conn.cursor()
+
+
 def create_table():
     c.execute('CREATE TABLE feedback_data(name TEXT, email TEXT,phone INTEGER, gender TEXT,course TEXT,rate INTEGER)')
     return True
 
+
 create_table()
-print("succesfully created")
+print("successfully created")
